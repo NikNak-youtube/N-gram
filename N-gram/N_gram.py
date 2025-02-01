f3c0d00cacb4fba534644dfd0a3ff81e1b4b4eb4
Nak Nik <isaac.askeland@icloud.com>
2025-01-31 23:23:36 -0800

    Update text processing to use 5-grams instead of 3-grams
    Modified loop_text and generate_text functions to handle 5-grams.
    Commented out original 3-gram versions of these functions.
    The nochar variable remains unchanged.

--- N-gram/N_gram.py
+++ N-gram/N_gram.py
@@ -13,15 +13,27 @@ nochar = ".,!@#$%^&*()_+{}|:<>?`-=[]\';/1234567890"
 
 word_counter = Counter()
 word_list={}
+# n of 5
 def loop_text(text):
     words = text.split()
-    for i in range(2, len(words)-1):
-        key = (words[i-2].lower().strip(nochar), words[i-1].lower().strip(nochar), words[i].lower().strip(nochar))
+    for i in range(4, len(words)-1):
+        key = (words[i-4].lower().strip(nochar), words[i-3].lower().strip(nochar), words[i-2].lower().strip(nochar), words[i-1].lower().strip(nochar), words[i].lower().strip(nochar))
         if key not in word_list:
             word_list[key] = [words[i+1]]
         else:
             word_list[key].append(words[i+1])
         word_counter[key] += 1
+
+# n of 3
+#def loop_text(text):
+#    words = text.split()
+#    for i in range(2, len(words)-1):
+#        key = (words[i-2].lower().strip(nochar), words[i-1].lower().strip(nochar), words[i].lower().strip(nochar))
+#        if key not in word_list:
+#            word_list[key] = [words[i+1]]
+#        else:
+#            word_list[key].append(words[i+1])
+#        word_counter[key] += 1
         
 def quick_loop_text(file_name):
     text = get_text(file_name)
@@ -37,19 +49,34 @@ for item in most_common:
     print(item[0])
     print(word_list[item[0]])
 
-# make a new generate_text function that allows for new versions of n in the n-gram
+# make a new generate_text function that allows for larger values of n
 # the function should generate text of a specified length
 
+# n of 5
 def generate_text(word_list, length=1000):
     words = list(word_list.keys())
     current_key = random.choice(words)
     for i in range(length):
-        print(current_key[2], end=' ')
-        current_key = (current_key[0].lower(), current_key[1].lower(), current_key[2].lower().strip(nochar))
+        print(current_key[4], end=' ')
+        current_key = (current_key[1].lower(), current_key[2].lower(), current_key[3].lower(), current_key[4].lower().strip(nochar))
         next_words = word_list.get(current_key, None)
         if not next_words:
             break
         next_word = random.choice(next_words)
-        current_key = (current_key[1].lower().strip(nochar), current_key[2].lower().strip(nochar), next_word)
+        current_key = (current_key[1].lower().strip(nochar), current_key[2].lower().strip(nochar), current_key[3].lower().strip(nochar), current_key[4].lower().strip(nochar), next_word)
+    
+
+# n of 3
+#def generate_text(word_list, length=1000):
+#    words = list(word_list.keys())
+#    current_key = random.choice(words)
+#    for i in range(length):
+#        print(current_key[2], end=' ')
+#        current_key = (current_key[0].lower(), current_key[1].lower(), current_key[2].lower().strip(nochar))
+#        next_words = word_list.get(current_key, None)
+#        if not next_words:
+#            break
+#        next_word = random.choice(next_words)
+#        current_key = (current_key[1].lower().strip(nochar), current_key[2].lower().strip(nochar), next_word)
 generate_text(word_list)
     
